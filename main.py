a3a7482d0bb13bbe6bedb08de28377bcbdb3d79b
Jesse Terblanche <jessenterblanche@gmail.com>
2021-07-08 19:31:40 +0200

    update

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from tkinter import *
 from tkinter.font import Font
 from tkinter import messagebox
 import mysql.connector
-from tkinter.ttk import Combobox
+from tkinter.ttk import Combobox, Treeview
 from datetime import datetime
 import rsaidnumber
 
@@ -280,6 +280,327 @@ class Login:
 
         signoutbtn = Button(aframe, text='Sign Out', command=asignout)
         signoutbtn.place(x=120, y=100)
+
+        def editusers():
+            escreen = Toplevel()
+            escreen.geometry('910x600')
+            escreen.resizable(0, 0)
+
+            def dataselect(event=None):
+                if users.selection() == ():
+                    pass
+                else:
+                    edata = users.focus()
+                    items = users.item(edata)
+                    items = items['values']
+                    enameentry.delete(0, END)
+                    esurnameentry.delete(0, END)
+                    enumberentry.delete(0, END)
+                    epasswordentry.delete(0, END)
+                    enextofkinnameentry.delete(0, END)
+                    enextofkinnoentry.delete(0, END)
+                    enameentry.insert(0, items[1])
+                    esurnameentry.insert(0, items[2])
+                    enumberentry.insert(0, str(items[3]))
+                    epasswordentry.insert(0, items[4])
+                    eroleset.set(items[5])
+                    enextofkinnameentry.insert(0, items[6])
+                    enextofkinnoentry.insert(0, str(items[7]))
+
+            users = Treeview(escreen)
+            users['columns'] = (1, 2, 3, 4, 5, 6, 7, 8)
+            users.column('#0', width=0, stretch=NO)
+            users.column(2, anchor=CENTER, width=100)
+            users.column(3, anchor=CENTER, width=100)
+            users.column(1, anchor=CENTER, width=100)
+            users.column(4, anchor=CENTER, width=100)
+            users.column(5, anchor=CENTER, width=100)
+            users.column(6, anchor=CENTER, width=100)
+            users.column(7, anchor=CENTER, width=100)
+            users.column(8, anchor=CENTER, width=100)
+
+            users.heading('#0', text='', anchor=CENTER)
+            users.heading(1, text='ID', anchor=CENTER)
+            users.heading(2, text='Name', anchor=CENTER)
+            users.heading(3, text='Surname', anchor=CENTER)
+            users.heading(4, text='Contact No.', anchor=CENTER)
+            users.heading(5, text='Password', anchor=CENTER)
+            users.heading(6, text='Role', anchor=CENTER)
+            users.heading(7, text='Next Of Kin Name', anchor=CENTER)
+            users.heading(8, text='Next Of Kin No.', anchor=CENTER)
+
+            def tvinsert():
+                mycursor.execute('SELECT users.*, next_of_kin.Name, next_of_kin.Phone_number FROM users '
+                                 'INNER JOIN next_of_kin ON users.ID = next_of_kin.ID')
+                records = mycursor.fetchall()
+                for i in range(len(records)):
+                    users.insert(parent='', index=i, iid=i, text='', values=records[i])
+
+            users.bind('<ButtonRelease>', dataselect)
+
+            tvinsert()
+
+            users.place(x=25, y=50)
+
+            addframe = Frame(escreen, width=400, height=300, bg='white', highlightbackground="black", highlightthickness=1)
+            addframe.place(x=1000, y=1000)
+            addheading = Label(addframe, text='---ADD A USER---', bg='white')
+            addheading.place(x=15, y=17)
+            yourdetails = Label(addframe, text='_Your Details:_', bg='white')
+            yourdetails.place(x=20, y=40)
+            aidlabel = Label(addframe, text='ID', bg='white')
+            aidlabel.place(x=20, y=70)
+            aidentry = Entry(addframe, highlightbackground='black', highlightthickness=1, width=14)
+            aidentry.place(x=20, y=90)
+            anamelabel = Label(addframe, text='Name', bg='white')
+            anamelabel.place(x=150, y=70)
+            anameentry = Entry(addframe, highlightbackground='black', highlightthickness=1, width=14)
+            anameentry.place(x=150, y=90)
+            asurnamelabel = Label(addframe, text='Surname', bg='white')
+            asurnamelabel.place(x=280, y=70)
+            asurnameentry = Entry(addframe, highlightbackground='black', highlightthickness=1, width=14)
+            asurnameentry.place(x=280, y=90)
+            anumberlabel = Label(addframe, text='Contact Number', bg='white')
+            anumberlabel.place(x=20, y=130)
+            anumberentry = Entry(addframe, highlightbackground='black', highlightthickness=1, width=14)
+            anumberentry.place(x=20, y=150)
+            apasswordlabel = Label(addframe, text='Password', bg='white')
+            apasswordlabel.place(x=150, y=130)
+            apasswordentry = Entry(addframe, highlightbackground='black', highlightthickness=1, width=14)
+            apasswordentry.place(x=150, y=150)
+            arolelabel = Label(addframe, text='Role', bg='white')
+            arolelabel.place(x=280, y=130)
+            aroleset = StringVar(escreen)
+            aroleset.set("Select your role")
+            arolecombolist = ['ADMIN', 'STUDENT', 'LECTURER', 'GUEST']
+            aroleselector = Combobox(addframe, textvariable=aroleset, width=14)
+            aroleselector['values'] = arolecombolist
+            aroleselector['state'] = 'readonly'
+            aroleselector.place(x=280, y=150)
+            akindetails = Label(addframe, text='_Next Of Kin Details:_', bg='white')
+            akindetails.place(x=20, y=190)
+            anextofkinnamelabel = Label(addframe, text='Name', bg='white')
+            anextofkinnamelabel.place(x=20, y=220)
+            anextofkinnameentry = Entry(addframe, highlightbackground='black', highlightthickness=1, width=14)
+            anextofkinnameentry.place(x=20, y=240)
+            anextofkinnolabel = Label(addframe, text='Contact Number', bg='white')
+            anextofkinnolabel.place(x=150, y=220)
+            anextofkinnoentry = Entry(addframe, highlightbackground='black', highlightthickness=1, width=14)
+            anextofkinnoentry.place(x=150, y=240)
+
+            def adduser():
+                try:
+                    phonevalid = int(anumberentry.get())
+                    kphonevalid = int(anextofkinnoentry.get())
+                    if (aidentry.get() == '' or anameentry.get() == '' or asurnameentry.get() == ''
+                            or anumberentry.get() == '' or apasswordentry.get() == '' or aroleset.get() == 'Select your role'
+                            or anextofkinnameentry.get() == '' or anextofkinnoentry.get() == ''):
+                        messagebox.showerror('Entries Unfilled', 'Please fill out all entries before registering user.')
+                    else:
+                        if len(anumberentry.get()) != 10 and len(anextofkinnoentry.get()) != 10:
+                            messagebox.showerror('Invalid Contact Number',
+                                                 'Length of contact number must be 10 digits.')
+                        else:
+                            idno = rsaidnumber.parse(aidentry.get())
+                            if idno.valid is False:
+                                messagebox.showerror('Invalid ID', 'Please enter a valid SA ID.')
+                            else:
+                                if str.isalpha(anameentry.get()) is False or str.isalpha(anextofkinnameentry.get()) is False \
+                                        or str.isalpha(asurnameentry.get()) is False:
+                                    messagebox.showerror('Invalid Name/Surname',
+                                                         'Please enter only alphabetic characters for name')
+                                else:
+                                    sql1 = "INSERT INTO users VALUES (%s, %s, %s, %s, %s, %s)"
+                                    val1 = (aidentry.get(), anameentry.get(), asurnameentry.get(), anumberentry.get(),
+                                            apasswordentry.get(), aroleset.get())
+                                    sql2 = "INSERT INTO next_of_kin (ID, Name, Phone_number) VALUES (%s, %s, %s)"
+                                    val2 = (aidentry.get(), anextofkinnameentry.get(), anextofkinnoentry.get())
+                                    mycursor.execute(sql1, val1)
+                                    mycursor.execute(sql2, val2)
+                                    mydb.commit()
+                                    messagebox.showinfo("Successful", "Account is registered.")
+
+                                    users.delete(*users.get_children())
+                                    tvinsert()
+                except ValueError:
+                    messagebox.showerror('Invalid details', 'Phone number must be in digits only.')
+
+            adduserbtn = Button(addframe, text='Commit', command=adduser)
+            adduserbtn.place(x=280, y=235)
+
+            editframe = Frame(escreen, width=400, height=300, bg='white', highlightbackground="black", highlightthickness=1)
+            editframe.place(x=1000, y=1000)
+            editheading = Label(editframe, text='---EDIT A USER---', bg='white')
+            editheading.place(x=15, y=17)
+            eyourdetails = Label(editframe, text='_Your Details:_', bg='white')
+            eyourdetails.place(x=20, y=40)
+            enamelabel = Label(editframe, text='Name', bg='white')
+            enamelabel.place(x=20, y=70)
+            enameentry = Entry(editframe, highlightbackground='black', highlightthickness=1, width=14)
+            enameentry.place(x=20, y=90)
+            esurnamelabel = Label(editframe, text='Surname', bg='white')
+            esurnamelabel.place(x=150, y=70)
+            esurnameentry = Entry(editframe, highlightbackground='black', highlightthickness=1, width=14)
+            esurnameentry.place(x=150, y=90)
+            enumberlabel = Label(editframe, text='Contact Number', bg='white')
+            enumberlabel.place(x=280, y=70)
+            enumberentry = Entry(editframe, highlightbackground='black', highlightthickness=1, width=14)
+            enumberentry.place(x=280, y=90)
+            epasswordlabel = Label(editframe, text='Password', bg='white')
+            epasswordlabel.place(x=20, y=130)
+            epasswordentry = Entry(editframe, highlightbackground='black', highlightthickness=1, width=14)
+            epasswordentry.place(x=20, y=150)
+            erolelabel = Label(editframe, text='Role', bg='white')
+            erolelabel.place(x=150, y=130)
+            eroleset = StringVar(escreen)
+            eroleset.set("")
+            erolecombolist = ['ADMIN', 'STUDENT', 'LECTURER', 'GUEST']
+            eroleselector = Combobox(editframe, textvariable=eroleset, width=14)
+            eroleselector['values'] = erolecombolist
+            eroleselector['state'] = 'readonly'
+            eroleselector.place(x=150, y=150)
+            ekindetails = Label(editframe, text='_Next Of Kin Details:_', bg='white')
+            ekindetails.place(x=20, y=190)
+            enextofkinnamelabel = Label(editframe, text='Name', bg='white')
+            enextofkinnamelabel.place(x=20, y=220)
+            enextofkinnameentry = Entry(editframe, highlightbackground='black', highlightthickness=1, width=14)
+            enextofkinnameentry.place(x=20, y=240)
+            enextofkinnolabel = Label(editframe, text='Contact Number', bg='white')
+            enextofkinnolabel.place(x=150, y=220)
+            enextofkinnoentry = Entry(editframe, highlightbackground='black', highlightthickness=1, width=14)
+            enextofkinnoentry.place(x=150, y=240)
+
+            def edituser():
+                edata = users.focus()
+                items = users.item(edata)
+                items = items['values']
+                try:
+                    phonevalid = int(enumberentry.get())
+                    kphonevalid = int(enextofkinnoentry.get())
+                    if (enameentry.get() == '' or esurnameentry.get() == ''
+                            or enumberentry.get() == '' or epasswordentry.get() == '' or eroleset.get() == ''
+                            or enextofkinnameentry.get() == '' or enextofkinnoentry.get() == ''):
+                        messagebox.showerror('Entries Unfilled', 'Please fill out all entries before registering user.')
+                    else:
+                        if len(enumberentry.get()) != 10 and len(enextofkinnoentry.get()) != 10:
+                            messagebox.showerror('Invalid Contact Number',
+                                                 'Length of contact number must be 10 digits.')
+                        else:
+                            if str.isalpha(enameentry.get()) is False or str.isalpha(enextofkinnameentry.get()) is False \
+                                    or str.isalpha(esurnameentry.get()) is False:
+                                messagebox.showerror('Invalid Name/Surname',
+                                                     'Please enter only alphabetic characters for name')
+                            else:
+                                sql1 = "UPDATE users SET Name=%s, Surname=%s, Phone_number=%s" \
+                                       "Password=%s, Role=%s WHERE ID='" + str(items[0]) + "'"
+                                val1 = (enameentry.get(), esurnameentry.get(), enumberentry.get(),
+                                        epasswordentry.get(), eroleset.get())
+                                sql2 = "UPDATE next_of_kin SET Name=%s, Phone_number=%s" \
+                                       "WHERE ID='" + str(items[0]) + "'"
+                                val2 = (enextofkinnameentry.get(), enextofkinnoentry.get())
+                                mycursor.execute(sql1, val1)
+                                mycursor.execute(sql2, val2)
+                                mydb.commit()
+                                messagebox.showinfo("Successful", "Account details updated.")
+
+                                users.delete(*users.get_children())
+                                tvinsert()
+                except ValueError:
+                    messagebox.showerror('Invalid details', 'Phone number must be in digits only.')
+
+            edituserbtn = Button(editframe, text='Commit', command=edituser)
+            edituserbtn.place(x=280, y=235)
+            """
+            id, name, surname, contactno, password, role, nextokin name, nextokin contactno
+            """
+
+            def adduserfunc():
+                editframe.place(x=1000, y=1000)
+                addframe.place(x=200, y=290)
+
+            addbtn = Button(escreen, text='Add user', command=adduserfunc)
+            addbtn.place(x=50, y=350)
+
+            def edituserfunc():
+                addframe.place(x=1000, y=1000)
+                editframe.place(x=200, y=290)
+
+            edbtn = Button(escreen, text='Edit user', command=edituserfunc)
+            edbtn.place(x=50, y=400)
+
+            def deleteuser():
+                editframe.place(x=1000, y=1000)
+                edata = users.focus()
+                items = users.item(edata)
+                items = items['values']
+                mycursor.execute("DELETE FROM next_of_kin WHERE ID='" + str(items[0]) + "'")
+                mycursor.execute("DELETE FROM users WHERE ID='" + str(items[0]) + "'")
+                mydb.commit()
+                users.delete(*users.get_children())
+                tvinsert()
+
+            delbtn = Button(escreen, text='Delete user', command=deleteuser)
+            delbtn.place(x=50, y=450)
+
+            escreen.mainloop()
+
+        modifybtn = Button(aframe, text='Edit database', command=editusers)
+        modifybtn.place(x=70, y=50)
+
+        def logprivilege():
+            lscreen = Toplevel()
+            lscreen.geometry('500x500')
+            lscreen.resizable(0, 0)
+
+            si_users = Treeview(lscreen)
+            si_users['columns'] = (1, 2, 3, 4, 5, 6)
+            si_users.column('#0', width=0, stretch=NO)
+            si_users.column(2, anchor=CENTER, width=80)
+            si_users.column(3, anchor=CENTER, width=80)
+            si_users.column(1, anchor=CENTER, width=80)
+            si_users.column(4, anchor=CENTER, width=80)
+            si_users.column(5, anchor=CENTER, width=80)
+            si_users.column(6, anchor=CENTER, width=80)
+
+            si_users.heading('#0', text='', anchor=CENTER)
+            si_users.heading(1, text='Signed In', anchor=CENTER)
+            si_users.heading(2, text='ID', anchor=CENTER)
+            si_users.heading(3, text='Sign in date', anchor=CENTER)
+            si_users.heading(4, text='Sign in time', anchor=CENTER)
+            si_users.heading(5, text='Sign out date', anchor=CENTER)
+            si_users.heading(6, text='Sign out time', anchor=CENTER)
+
+            def tvinsert():
+                mycursor.execute('SELECT * FROM Signin')
+                records = mycursor.fetchall()
+                for i in range(len(records)):
+                    si_users.insert(parent='', index=i, iid=i, text='', values=records[i])
+
+            tvinsert()
+            si_users.place(x=25, y=50)
+
+            def loguserout():
+                select = str(int(si_users.selection()[0]) + 1)
+                so_tcurrent = datetime.now().time().strftime('%H:%M:%S')
+                so_dcurrent = datetime.now().date().strftime('%Y-%m-%d')
+                lsql = "UPDATE signin SET Sign_out_date=%s, Sign_out_time=%s WHERE SignedIn='" \
+                             + select + "' AND Sign_in_date='" + si_dcurrent + "'"
+                lval = (so_dcurrent, so_tcurrent)
+                mycursor.execute(lsql, lval)
+                mydb.commit()
+                si_users.delete(*si_users.get_children())
+                mycursor.execute('SELECT * FROM Signin')
+                records = mycursor.fetchall()
+                for i in range(len(records)):
+                    si_users.insert(parent='', index=i, iid=i, text='', values=records[i])
+
+            logoutbtn = Button(lscreen, text='Log user out', command=loguserout)
+            logoutbtn.place(x=130, y=300)
+
+            lscreen.mainloop()
+
+        logbtn = Button(aframe, text='Log users out', command=logprivilege)
+        logbtn.place(x=140, y=50)
 
         asiscreen.mainloop()
 
--- thesquare.py
+++ thesquare.py
@@ -7,6 +7,16 @@ biglist = [(123, 456, 789), (123, 456, 789), (123, 456, 789), (123, 456, 789)]
 root = Tk()
 root.geometry('400x400')
 
+def testfunc():
+    print(int(tv.selection()[0]) + 1)
+
+def testfunc2():
+    if tv.selection() == ():
+        print('none')
+    else:
+        print(tv.selection())
+
+
 tv = Treeview(root)
 tv['columns'] = ('number1', 'number2', 'number3')
 tv.column('#0', width=0, stretch=NO)
@@ -23,6 +33,9 @@ tv.insert(parent='', index=0, iid=0, text='', values=biglist[0])
 tv.insert(parent='', index=1, iid=1, text='', values=biglist[1])
 tv.insert(parent='', index=2, iid=2, text='', values=biglist[2])
 tv.insert(parent='', index=3, iid=3, text='', values=biglist[3])
+
+xx = Button(root, text='test', command=testfunc2)
+xx.pack()
 
 tv.pack()
 
